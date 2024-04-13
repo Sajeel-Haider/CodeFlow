43f80e8285ca0226200a0af0521665f2b6297b6b
Sajeel-Haider <sajeelshp1@gmail.com>
2024-04-13 16:56:06 +0500

    * added admin dashboard template

--- codeflow-frontend/src/pages/Dashboard/AdminDashboard/AdminDashboard.js
+++ codeflow-frontend/src/pages/Dashboard/AdminDashboard/AdminDashboard.js
@@ -1,5 +1,20 @@
+import Sidebar from "../../../components/Dashboard/AdminDashboard/Sidebar";
+import { SidebarItem } from "../../../components/Dashboard/AdminDashboard/Sidebar";
+import { IoMdSettings } from "react-icons/io";
+import { MdSpaceDashboard } from "react-icons/md";
+import { ImStatsDots } from "react-icons/im";
 const AdminDashboard = () => {
-  return <div>AdminDashboard</div>;
+  return (
+    <div className="flex flex-row">
+      <Sidebar>
+        <SidebarItem icon={<MdSpaceDashboard />} text="Dashboard" />
+        <SidebarItem icon={<ImStatsDots />} text="Statistics" />
+        <hr className="my-3" />
+        <SidebarItem icon={<IoMdSettings />} text="Settings" />
+      </Sidebar>
+      <div className="p-4">hello</div>
+    </div>
+  );
 };
 
 export default AdminDashboard;
--- codeflow-frontend/src/pages/Dashboard/UserDashboard/UserDashboard.js
+++ codeflow-frontend/src/pages/Dashboard/UserDashboard/UserDashboard.js
@@ -1,5 +1,25 @@
+import Sidebar, {
+  SidebarItem,
+} from "../../../components/Dashboard/AdminDashboard/Sidebar";
+import { FaCode } from "react-icons/fa6";
+import { MdSpaceDashboard } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
+import { IoIosGitNetwork } from "react-icons/io";
+import { MdQuestionMark } from "react-icons/md";
 const UserDashboard = () => {
-  return <div>UserDashboard</div>;
+  return (
+    <div className="flex flex-row">
+      <Sidebar>
+        <SidebarItem icon={<MdSpaceDashboard />} text="Dashboard" />
+        <SidebarItem icon={<IoIosGitNetwork />} text="Repositories" />
+        <SidebarItem icon={<FaCode />} text="Problems" />
+        <SidebarItem icon={<MdQuestionMark />} text="About us" />
+        <hr className="my-3" />
+        <SidebarItem icon={<IoMdSettings />} text="Settings" />
+      </Sidebar>
+      <div className="p-4">hello</div>
+    </div>
+  );
 };
 
 export default UserDashboard;
